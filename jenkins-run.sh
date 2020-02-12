e4ef5d7c9ebb51dcdba9524d380174f5374fd1e7
rlp243 <rlp243@cornell.edu>
2020-02-12 16:04:20 -0500

    first update pulling jenkins out of main and putting into new repo

---
+++ jenkins-run.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#-----------------------------------------------------------------------------#
+### Tagging images
+# This script will pull retag and push all images within the EBS operating 
+# environment
+
+#-----------------------------------------------------------------------------#
+#@author: RLPetrie (rlp243@cornell.edu)
+
+pwd="`pwd`" && echo $pwd
+
+#-----------------------------------------------------------------------------#
+
+docker stop jenkins-node || true && docker rm jenkins-node || true
+
+#-----------------------------------------------------------------------------#
+
+
+docker run -dti \
+--name jenkins-node \
+-h jenkins-node \
+-v $pwd:/var/jenkins_home \
+-p 8080:8080 \
+-p 50000:50000 \
+jenkins/jenkins:lts
+echo;
+
+#-----------------------------------------------------------------------------#
+
+echo "Please Copy the following line into localhost:8080"
+
+docker exec jenkins-node bash -c "
+cat /var/jenkins_home/secrets/initialAdminPassword
+"
+
+#-----------------------------------------------------------------------------#
+### Install vim
+
+docker exec -u root jenkins-node bash -c "
+apt update && apt install vim tree htop -y
+"
+
