c39e9905a62f08c021ba2e2b737d93f90acc4a16
rlp243 <rlp243@cornell.edu>
2020-08-18 11:17:26 -0400

    update

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -7,22 +7,45 @@
 #-----------------------------------------------------------------------------#
 #@author: RLPetrie (rlp243@cornell.edu)
 
+#-----------------------------------------------------------------------------#
+### verify proper arguments are passed
+
+if [ $# -lt 2 ];
+  then
+    echo "No arguments supplied."
+    echo "Usage: bash <bash_script_name>.sh <NDD_PATH> <JENKINS_EXT_PORT>"
+    echo "/data directory is mounted which is normally a symlink which needs 
+    to be mounted for the symlink pointer"
+    exit 1
+fi
+
+#-----------------------------------------------------------------------------#
+### set present working directory (jenkins repo) as jenkins_home
+
 pwd="`pwd`" && echo $pwd
 
 #-----------------------------------------------------------------------------#
 
-docker stop jenkins-node || true && docker rm jenkins-node || true
+NDD_PATH=$1
+JENKINS_EXT_PORT=$2
+
+#-----------------------------------------------------------------------------#
+
+docker rm -f jenkins-node || true
 
 #-----------------------------------------------------------------------------#
 
 docker run -dti \
 --name jenkins-node \
 -h jenkins-node \
+-v ${NDD_PATH}:${NDD_PATH} \
+-v /data:/data \
 -v $pwd:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
--p 8090:8080 \
+-p $JENKINS_EXT_PORT:8080 \
 -p 50000:50000 \
-jenkins/jenkins:lts
+--restart=always \
+techie624/petrie.jenkins:v0.1
 echo;
 
 #-----------------------------------------------------------------------------#
@@ -30,23 +53,23 @@ echo;
 
 docker exec -u root jenkins-node bash -c "
 apt update && apt install vim tree htop sudo -y;
-apt-get update;
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y;
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -;
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable";
-apt-get update;
-apt-get install docker-ce docker-ce-cli containerd.io -y;
-echo 'jenkins ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-usermod -aG docker jenkins
 "
+echo;
 
 #-----------------------------------------------------------------------------#
+### On initial setup display initialAdminPassword
+
+docker exec -u root jenkins-node bash -c '
+FILE="/var/jenkins_home/secrets/initialAdminPassword"
+if [[ -f "$FILE" ]]; then
+   echo "Please Copy the following line into localhost:8080"
+   cat /var/jenkins_home/secrets/initialAdminPassword
+   echo;
+fi';
 echo;
 
-echo "Please Copy the following line into localhost:8080"
-echo;
+#-----------------------------------------------------------------------------#
+### Script completion
 
-cat /var/jenkins_home/secrets/initialAdminPassword
+echo "Jenkins Deployment script has completed! Good Bye."
+echo;
